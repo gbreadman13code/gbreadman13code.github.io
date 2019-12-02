d7afc3a533969c37e3b1c0bc08093bcf2e90a129
Илья Филонов <you@example.com>
2019-12-02 22:16:22 +0700

    vsd

--- hermes/js/main.js
+++ hermes/js/main.js
@@ -45,5 +45,8 @@ $(document).ready(function () {
     $('.mobile-menu-link').click(function (e) { 
         e.preventDefault();
         $('.all-wrap').toggleClass('slide');
+        $('.burger-menu-line1').toggleClass('burger-menu-line1-active');
+        $('.burger-menu-line2').toggleClass('burger-menu-line2-active');
+        $('.burger-menu-line3').toggleClass('burger-menu-line3-active');
     });
 });
